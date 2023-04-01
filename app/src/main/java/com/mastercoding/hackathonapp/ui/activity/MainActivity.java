fd588c4e9bc4caca6a9a1cdc1504c357c8c88c4f
Sarnaz Hossain <hsarnaz90@icloud.com>
2023-04-01 15:06:36 +0100

    directory structure

--- app/src/main/java/com/mastercoding/hackathonapp/ui/activity/MainActivity.java
+++ app/src/main/java/com/mastercoding/hackathonapp/ui/activity/MainActivity.java
@@ -1,11 +1,12 @@
-package com.mastercoding.hackathonapp;
+package com.mastercoding.hackathonapp.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.mastercoding.hackathonapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
