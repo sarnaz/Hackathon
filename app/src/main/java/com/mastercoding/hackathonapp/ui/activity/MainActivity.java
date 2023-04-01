9ce08a729edcff615601b97bbac03b04ccc76d4c
blackout-03 <resul13@outlook.com>
2023-04-01 16:10:38 +0100

    Add Bottom Navigation Bar to MainActivity

--- app/src/main/java/com/mastercoding/hackathonapp/ui/activity/MainActivity.java
+++ app/src/main/java/com/mastercoding/hackathonapp/ui/activity/MainActivity.java
@@ -1,26 +1,57 @@
 package com.mastercoding.hackathonapp.ui.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.mastercoding.hackathonapp.R;
+import com.mastercoding.hackathonapp.ui.fragments.HomeFragment;
+import com.mastercoding.hackathonapp.ui.fragments.RecommendationFragment;
+import com.mastercoding.hackathonapp.ui.fragments.ScannerFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn = findViewById(R.id.btn);
+        //Declaring the Views I'm adding functionality to - Resul
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_item_1:
+                        // Handle button 1 click
+                        startActivity(new Intent(MainActivity.this, HomeFragment.class));
+                        return true;
 
-        btn.setOnClickListener(v -> Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show());
+                    case R.id.menu_item_2:
+                        // Handle button 2 click
+                        startActivity(new Intent(MainActivity.this, RecommendationFragment.class));
+                        return true;
 
+                        case R.id.menu_item_3:
+                        // Handle button 3 click
+                        startActivity(new Intent(MainActivity.this, ScannerFragment.class));
+                        return true;
+
+                        default:
+                        return false;
+                }
+            }
+        });
 
 
     }
