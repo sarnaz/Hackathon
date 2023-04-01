277517f1fbc4720e8493efd3171170eaba8029b5
blackout-03 <resul13@outlook.com>
2023-04-01 14:31:26 +0100

    Add button

---
+++ app/src/main/java/com/mastercoding/hackathonapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.mastercoding.hackathonapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(v -> Toast.makeText(MainActivity.this, "Button Clicked", Toast.LENGTH_SHORT).show());
+
+
+
+    }
+}
